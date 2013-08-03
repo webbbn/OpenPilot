40d68c3ad419de028f19c06852a6c3412b4b716b
Brian Webb <webbbn@gmail.com>
2013-08-03 07:51:49 -0700

    Crazyflie: Fixes the firmware bank size.

--- flight/targets/boards/crazyflie/board-info.mk
+++ flight/targets/boards/crazyflie/board-info.mk
@@ -16,7 +16,7 @@ OPENOCD_CONFIG      := stm32f1x.stlink.cfg
 BL_BANK_BASE        := 0x08000000  # Start of bootloader flash
 BL_BANK_SIZE        := 0x00003000  # Should include BD_INFO region
 FW_BANK_BASE        := 0x08003000  # Start of firmware flash
-FW_BANK_SIZE        := 0x0001D000  # Should include FW_DESC_SIZE
+FW_BANK_SIZE        := 0x00017000  # Should include FW_DESC_SIZE
 EE_BANK_BASE        := 0x0801A000  # EEPROM storage area
 EE_BANK_SIZE        := 0x00006000  # Size of EEPROM storage area
 
